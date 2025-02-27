cd1618e5f1d308ed9d1f3be83bd2cecce65b6b76
Sigurd Torve Sæther <torve_s@live.no>
2025-02-27 20:39:07 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import pyaudio
+import wave
+import sys  
+import keyboard
+
+
+if len(sys.argv) < 2:
+   print(f'Writes a wave file. Usage: {sys.argv[0]} filename.wav')
+   sys.exit(-1) 
+
+
+def record(filename):
+    with wave.open(f'./written/{sys.argv[1]}', 'wb') as wf:
+        p = pyaudio.PyAudio()
+        framerate = int(p.get_default_input_device_info().get("defaultSampleRate"))
+        sampwidth = p.get_sample_size(pyaudio.paInt16)
+        format = p.get_format_from_width(sampwidth)
+        channels = 1
+        wf.setparams((channels, sampwidth, framerate, 0, 'NONE', 'not compressed')) 
+        stream = p.open(framerate, channels, format, input=True)
+        print('* recording... Press enter to stop recording')
+
+        while True:
+            if keyboard.is_pressed('enter'):
+                break
+            data = stream.read(framerate)
+            wf.writeframes(data)
+
+        stream.close()
+        p.terminate()
+        print('* done recording')
+
+print('Press enter to start recording')
+keyboard.wait('enter')
+print('Recording...')
+record(sys.argv[1])
